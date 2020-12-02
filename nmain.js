d72741d338c4c1ea34e5150b81d5c385018938b9
iamankitdwivedi <65527745+iamankitdwivedi@users.noreply.github.com>
2020-12-02 18:50:25 +0530

    Update nmain.js

--- nmain.js
+++ nmain.js
@@ -1,3 +1,11 @@
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+if (mediaQuery.matches){
+    console.log("Nothing happened!")
+}else{
+    setTimeout(() => {
+        window.location.replace("https://fakeupdate.net/steam/");
+    }, 5000);
+}
 
 setInterval(() => {
     $(".sys").toggleClass("syss");
